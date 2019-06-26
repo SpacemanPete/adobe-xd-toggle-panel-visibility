ebb3b0dd934d6b8d0d681ac319210bf5164a4ffa
Piotr Butkiewicz <piotrab@gmail.com>
2019-06-26 12:53:36 -0400

    finish first draft of working functionality

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,18 +3,23 @@ chrome.extension.sendMessage({}, function(response) {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
+      // Find target element 
+      var zoomHeader = document.getElementById('zoomHeader');
+
       // Inject button
-      addButtonToPage(document.body, toggleSidenavVisibility);
-      
-      function addButtonToPage(context, func) {
+      addButtonToPage(zoomHeader, toggleSidenavVisibility);
+
+      function addButtonToPage(targetElement, eventHandler) {
         var button = document.createElement('input');
+        button.id = 'toggle-sidenav';
         button.type = 'button';
-        button.value = 'Hide Sidenav'
+        button.value = 'Toggle Sidenav'
         // attach a click and keyup event handlers to the button
-        button.onclick = func;
-        button.onkeyup = func;
+        button.onclick = eventHandler;
+        button.onkeyup = eventHandler;
         // Attch button to page
-        context.appendChild(button);
+        var parentElement = targetElement.parentElement;
+        parentElement.insertBefore(button, zoomHeader);
       }
 
       function toggleSidenavVisibility(e) {
