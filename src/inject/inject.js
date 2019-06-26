5a7da39bb55f8b0e286bf59b9ccdbd3b5270de85
Piotr Butkiewicz <piotrab@gmail.com>
2019-06-26 12:22:03 -0400

    add sidenab visibility JS to inject script

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,30 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+      // Inject button
+      addButtonToPage(document.body, toggleSidenavVisibility);
+      
+      function addButtonToPage(context, func) {
+        var button = document.createElement('input');
+        button.type = 'button';
+        button.value = 'Hide Sidenav'
+        // attach a click and keyup event handlers to the button
+        button.onclick = func;
+        button.onkeyup = func;
+        // Attch button to page
+        context.appendChild(button);
+      }
 
-	}
+      function toggleSidenavVisibility(e) {
+        // toggle visibility on click
+        if ( e.type === 'click' || e.keyCode ==='13' ) {
+          var sidenav = document.querySelectorAll("[data-auto='mySidenav']")[0];
+          sidenav.classList.toggle('hidden');
+        }
+      }
+
+    }
 	}, 10);
 });
